813bda402a20c42c7f0704838ecc4cefa6fcc073
Garmax <shuaiyux@gmail.com>
2020-12-03 23:26:26 +0800

    51&52

--- submission/generate_submission.py
+++ submission/generate_submission.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-experiment = 'bs50_lr5e-5_ep3_large'
+experiment = 'bs52_lr5e-5_ep3_large'
 
 template = pd.read_csv('../data/sample_submission.tsv', sep='\t',
                        header=None, dtype=np.int32)
